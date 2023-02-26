edfa8cd58e434ad0798d58a6affcce65982246a1
jakefortay <jakefortay@gmail.com>
2023-02-26 11:06:41 -0500

    Grading system

--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -19,6 +19,21 @@ public final class GeneticAlgorithm {
 
     /**
      *
+     * @param array
+     *
+     *            array to be printed
+     */
+    private static void print2D(char[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     *
      * @param lib
      *            Library of characters to choose from
      * @param popSize
@@ -29,22 +44,35 @@ public final class GeneticAlgorithm {
      *            Array to contain Strings
      */
     private static void genPop(String lib, int popSize, int targetSize,
-            String[] gen) {
+            char[][] gen) {
 
         int random;
 
         for (int i = 0; i < gen.length; i++) {
-            gen[i] = "";
+
             for (int j = 0; j < targetSize; j++) {
                 random = (int) Math.floor(Math.random() * (51 - 0 + 1) + 0);
-                gen[i] += lib.charAt(random);
+                gen[i][j] = lib.charAt(random);
             }
         }
 
     }
 
-    private static void gradePop() {
-
+    /**
+     *
+     * @param fitness
+     *            array of fitness scores
+     * @param gen
+     *            array of guesses
+     */
+    private static void gradePop(int[] fitness, char[][] gen, char[] target) {
+        for (int i = 0; i < gen.length; i++) {
+            for (int j = 0; j < gen[i].length; j++) {
+                if (gen[i][j] == target[j]) {
+                    fitness[i]++;
+                }
+            }
+        }
     }
 
     /**
@@ -60,22 +88,48 @@ public final class GeneticAlgorithm {
         String lib = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
         out.println("Enter a target string: ");
-        String target = in.nextLine();
+        String input = in.nextLine();
 
-        int size = target.length();
+        int wordSize = input.length();
+
+        char[] target = new char[wordSize];
+
+        for (int i = 0; i < target.length; i++) {
+            target[i] = input.charAt(i);
+        }
+
+        // Have string target converted to char array @target
 
         out.println("Enter a population size: ");
         int popSize = in.nextInteger();
 
-        String[] gen = new String[popSize];
+        char[][] gen = new char[popSize][wordSize];
+        int[] fitness = new int[popSize];
 
-        // Generate initial population 
+        // Generate initial population
 
-        genPop(lib, popSize, size, gen);
+        genPop(lib, popSize, wordSize, gen);
 
-        for (int i = 0; i < gen.length; i++) {
-            out.println(gen[i]);
+        print2D(gen);
+
+        gradePop(fitness, gen, target);
+
+        for (int i = 0; i < fitness.length; i++) {
+            System.out.println(fitness[i]);
         }
+
+        boolean running = true;
+
+//        while (running) {
+//
+//            // Grade the population, return array of scores, array of guesses
+//
+//            // Create a new array with the top half of the scorers, retain orginal
+//
+//            // Modify the original array by taking first x and last y # of chars
+//            // from stop scorer array
+//
+//        }
 
         in.close();
         out.close();
