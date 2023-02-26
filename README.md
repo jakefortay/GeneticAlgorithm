c885f01d3547160cdc75b3cfab637325fea93a16
jakefortay <jakefortay@gmail.com>
2023-02-26 08:46:09 -0500

    Population Setup

--- README.md
+++ README.md
@@ -3,3 +3,19 @@
 ##### Coming soon! 
 
 ##### Maybe! 
+
+Brainstorming 
+
+Goal : Binary String, 0s, and 1s
+
+Start : 0000000 |  0 
+        0100110 |  3 
+        0010011 |  3
+        
+        0101011 |  4 
+        0010100 |  2 
+        1010011 |  4
+        
+         
+
+End :   1111111
--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -18,6 +18,32 @@ public final class GeneticAlgorithm {
     }
 
     /**
+     *
+     * @param lib
+     *            Library of characters to choose from
+     * @param popSize
+     *            Population size of generation
+     * @param targetSize
+     *            Length of the target string
+     * @param gen
+     *            Array to contain Strings
+     */
+    private static void genPop(String lib, int popSize, int targetSize,
+            String[] gen) {
+
+        int random;
+
+        for (int i = 0; i < gen.length; i++) {
+            gen[i] = "";
+            for (int j = 0; j < targetSize; j++) {
+                random = (int) Math.floor(Math.random() * (51 - 0 + 1) + 0);
+                gen[i] += lib.charAt(random);
+            }
+        }
+
+    }
+
+    /**
      * Main method.
      *
      * @param args
@@ -27,7 +53,25 @@ public final class GeneticAlgorithm {
         SimpleReader in = new SimpleReader1L();
         SimpleWriter out = new SimpleWriter1L();
 
-        // First comment :) 
+        String lib = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        out.println("Enter a target string: ");
+        String target = in.nextLine();
+
+        int size = target.length();
+
+        out.println("Enter a population size: ");
+        int popSize = in.nextInteger();
+
+        String[] gen = new String[popSize];
+
+//        int random = (int) Math.floor(Math.random() * (10 - 0 + 1) + 0);
+
+        genPop(lib, popSize, size, gen);
+
+        for (int i = 0; i < gen.length; i++) {
+            out.println(gen[i]);
+        }
 
         in.close();
         out.close();
